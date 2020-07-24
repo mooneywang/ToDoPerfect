65f2dcf5addb84f250282d6c27d6b3ce155f8957
mooney_wang <837539840@qq.com>
2020-07-24 23:13:56 +0800

    代码初次提交

---
+++ ToDoPerfect/ToDoPerfect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoPerfect
+//
+//  Created by 王梦杰 on 2020/7/24.
+//  Copyright © 2020 MooneyWang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
